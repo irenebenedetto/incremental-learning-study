fd6a936f9d875cd617cad54db4722475257d0a08
m-pana <michele.panar@gmail.com>
2020-06-12 12:23:55 +0200

    Replaced SVM class
    With the working one, hopefully. I don't know. I'm tired. I've done way more than i should have for this project. I want a phd too

--- svmCaRL.py
+++ svmCaRL.py
@@ -1,29 +1,8 @@
 from sklearn.svm import SVC
-import torch
-from torch.utils.data import Subset, DataLoader, TensorDataset, Dataset
-from torch.backends import cudnn
-import torch.optim as optim
-from torchvision.transforms import Compose
-from torchvision import transforms
-import torchvision.models as models
-from PIL import Image
-from tqdm import tqdm
-import numpy as np
-import random
-import pandas as pd
-import os
-import torch.nn as nn
-import torch.utils.model_zoo as model_zoo
-import gc
-from copy import deepcopy
-from MLDL.utils import *
-from MLDL.datasets.project_dataset1 import MergeDataset
 
 class SVMiCaRL():
     """
-    Implements iCaRL as decribed in *insert paper* (the actual name of the paper is *insert paper*)
-
-    The behavior of "distillation" flag is overridden if a custom loss is used.
+    Implements iCaRL + SVM, i don't even know if it's the right one
     """
     def __init__(self, net, K=2000, custom_loss=None, loss_params=None, use_exemplars=True, distillation=True, all_data_means=True):
         self.exemplar_sets = []
@@ -128,7 +107,7 @@ class SVMiCaRL():
         for label, exemplar_set in enumerate(self.exemplar_sets):
             for exemplar in exemplar_set:
                 exemplars_dataset.append((exemplar, label))
-        
+
         num_old_classes = len(self.exemplar_sets)
         num_new_classes = len(np.unique(train_dataset.targets))
         num_tot_classes = num_old_classes + num_new_classes
@@ -139,78 +118,76 @@ class SVMiCaRL():
             D = MergeDataset(train_dataset, exemplars_dataset, augment2=False)
         else:
             D = train_dataset
-    
+
         # If this is not the first training, we save the old network
-        
+
         old_net = deepcopy(self.net)
 
         optimizer = optim.SGD(self.net.parameters(), lr=self.LR, weight_decay=self.WEIGHT_DECAY, momentum=self.MOMENTUM)
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.MILESTONE, gamma=self.GAMMA)
-        
-        criterion = nn.BCEWithLogitsLoss()
+
+        criterion = nn.BCEWithLogitsLoss(reduction='none')
 
         dataloader = DataLoader(D, batch_size=self.BATCH_SIZE, shuffle=True, num_workers=4, drop_last=False)
-        
+
         for epoch in range(self.NUM_EPOCHS):
             print(f'EPOCH {epoch+1}/{self.NUM_EPOCHS}, LR = {scheduler.get_last_lr()}')
 
-            mean_loss_epoch = 0      
+            mean_loss_epoch = 0
             for images, labels in dataloader:
                 images = images.to(self.DEVICE)
                 labels = labels.to(self.DEVICE)
-                
+
                 self.net.train()
                 optimizer.zero_grad()
 
-                loss = self.l2_loss(images, labels, old_net)
-                
+                outputs = self.net(images)[:, :num_tot_classes]
+
+                # One hot encoding labels for binary cross-entropy loss
+                labels_onehot = nn.functional.one_hot(labels, self.num_tot_classes).type_as(outputs)
+
+                if num_old_classes == 0 or not self.distillation:
+                    loss = criterion(outputs, labels_onehot).sum(dim=1).mean()
+                else:
+                    labels_onehot = labels_onehot.type_as(outputs)[:, num_old_classes:]
+                    out_old = torch.sigmoid(old_net(images))[:,:num_old_classes]
+                    target = torch.cat((out_old, labels_onehot), dim=1)
+                    loss = criterion(outputs, target).sum(dim=1).mean()
+
+                soft_margin_loss = self.soft_nearest_mean_class_loss(images, labels, old_net)
+
                 mean_loss_epoch += loss.item()
-                loss.backward()
+                total_loss = loss + soft_margin_loss
+                total_loss.backward()
                 optimizer.step()
                 # --- end batch
             scheduler.step()
             print(f"Mean batch loss: {mean_loss_epoch/len(dataloader):.5}")
             # --- end epoch
-            
+
         torch.cuda.empty_cache()
         return D
 
     def train_SVM(self, dataset):
-        """
-        The fucntion perform the training, after the network training on the linear svc
-        The svc classifier training function is called AFTER the training of the network
-        with 
-                            self.train_svc(dataloader)
-        Params:
-            X: the dataloader with new class images and exemplars
-        
-        """
         print('Training SVM...')
-
         svc = SVC(kernel='linear', tol=0.0001)
         dataloader = DataLoader(dataset, batch_size=100, shuffle=False, drop_last=False, num_workers=4)
 
         with torch.no_grad():
+            self.net.eval()
             labels_list = []
             fts_list = []
             for images, labels in dataloader:
                 fts_map = self.net.feature_extractor(images.to(self.DEVICE))
                 labels_list.append(labels)
                 fts_list.append(fts_map.cpu())
-            
+
             all_labels = torch.cat(labels_list)
             all_fts = torch.cat(fts_list)
             svc.fit(all_fts, all_labels)
             self.svc = svc
 
     def construct_exemplar_set_SVM(self, D, m):
-        """
-        The function builds the exemplar set with the support vectors returned by
-        the SVM.
-        Params:
-            D: the dataset, contains the new labels and the past exemplars stored
-            m: the number of support vectors required for each class
-        """
         support_indexes = self.svc.support_
 
         X = torch.stack([img for img, _ in D])
@@ -228,59 +205,90 @@ class SVMiCaRL():
             support_set = support_set[:m]
             self.exemplar_sets.append(support_set)
 
+    def train_SVM_for_classification(self):
+        """
+        Non prende niente a parametro.
+        Trains SVM only on exemlars for classification purposes.
+        Won't work.
+        """
+        # Concatenate current exemplar sets with respective labels
+        exemplars_dataset = []
+        for label, exemplar_set in enumerate(self.exemplar_sets):
+            for exemplar in exemplar_set:
+                exemplars_dataset.append((exemplar, label))
+
+        svc = SVC(kernel='linear', tol=0.0001)
+        dataloader = DataLoader(exemplars_dataset, batch_size=100, shuffle=False, drop_last=False, num_workers=4)
+        with torch.no_grad():
+            self.net.eval()
+            labels_list = []
+            fts_list = []
+            for images, labels in dataloader:
+                fts_map = self.net.feature_extractor(images.to(self.DEVICE))
+                labels_list.append(labels)
+                fts_list.append(fts_map.cpu())
+
+            all_labels = torch.cat(labels_list)
+            all_fts = torch.cat(fts_list)
+            svc.fit(all_fts, all_labels)
+            self.svc = svc
+
+    def soft_nearest_mean_class_loss(self, images, labels, old_net, T=2):
+        """
+        Compute soft nearest mean class loss, which has been proven to have the longest name in all loss functions history.
+        This is probably the only goal we'll achieve with that.
+
+        Returns:
+          loss as a scalar for the whole batch, ready to call backward on
+        """
+        self.net.eval()
+        X = self.net.feature_extractor(images)
+
+        all_logs = []
+        for i, x in enumerate(X):
+            bool_idx = torch.sum(X!=x, dim=1).type(torch.bool)
+            all_X_except_x = X[bool_idx]
+            all_distances_squared = (x - all_X_except_x).pow(2).sum(dim=1)
+            denominator = torch.exp(-all_distances_squared/T).sum()
+
+            all_y_except_x = labels[bool_idx]
+            X_same_label_as_x = all_X_except_x[all_y_except_x != labels[i]] # This is very readable
+            all_distances_squared = (x - X_same_label_as_x).pow(2).sum(dim=1)
+            numerator = torch.exp(-all_distances_squared/T).sum()
+
+            x_contribution_to_loss = torch.log(numerator/denominator)
+            all_logs.append(x_contribution_to_loss)
+        # Sum all contributions (outer sum)
+        b = images.size(0)
+        loss = - torch.Tensor(all_logs).sum() / b
+        return loss
+
+
+
     def incremental_train(self, train_dataset, train_dataset_no_aug, test_dataset):
         labels = train_dataset.targets
         new_classes = np.unique(labels)
         print(f'Arriving new classes {new_classes}')
-        
+
         # Compute number of total labels
         num_old_labels = len(self.exemplar_sets)
         num_new_labels = len(new_classes)
 
         t = num_old_labels + num_new_labels
         D = self.update_representation(train_dataset)
+        # SVM is first trained to select exemplars
         self.train_SVM(D)
         m = int(self.K/t)
         self.construct_exemplar_set_SVM(D, m)
         self.compute_exemplars_means()
 
+        # We now retrain it to classify based on exemplars only
+        self.train_SVM_for_classification()
+
+
         self.test_SVM(test_dataset)
         self.test_FC(test_dataset)
         self.test_NCM(test_dataset)
-    
-    def l2_loss(self, images, labels, old_net):
-        """
-        The function compute the l2 loss as distillation loss and a cross entropy loss for the classification task
-        Params:
-            images: to classify
-            labels
-            num_old_classes: number of old classes 
-            old_net: old network used to compute 
-        Returns:
-            the value of the total loss (distillation + classification)
-        """
-        outputs = self.net(images)[:, :self.num_tot_classes]
-        num_old_classes = len(self.exemplar_sets)
-      
-        if num_old_classes == 0:
-            cross_entropy = nn.CrossEntropyLoss()
-            loss = cross_entropy(outputs, labels)
-
-        else:
-            l2_loss = nn.MSELoss()          # l2 loss for distillation
-            CELoss = nn.CrossEntropyLoss()  # cross entropy loss for classification
-
-            fts_old = old_net.feature_extractor(images) 
-            fts_new = self.net.feature_extractor(images)
-            
-            s = nn.Softmax(dim=1)
-            
-            class_loss = CELoss(outputs, labels)
-            dist_loss_l2 = l2_loss(fts_new, fts_old)*40
-            
-            loss = class_loss + dist_loss_l2
-
-        return loss
 
     def test_NCM(self, test_dataset):
         self.net.eval()
@@ -294,14 +302,14 @@ class SVMiCaRL():
                 # print(f"Test labels: {np.unique(labels.numpy())}")
                 images = images.to(self.DEVICE)
                 labels = labels.to(self.DEVICE)
-                
+
                 # Get prediction with  NMC
                 preds = self.classify_NCM(images).to(self.DEVICE)
 
                 # Update Corrects
                 running_corrects += torch.sum(preds == labels.data).data.item()
                 update_confusion_matrix(matrix, preds, labels)
-        
+
             # Calculate Accuracy and mean loss
             accuracy = running_corrects / len(test_dataloader.dataset)
             self.accuracies_NCM.append(accuracy)
@@ -320,15 +328,15 @@ class SVMiCaRL():
                 # print(f"Test labels: {np.unique(labels.numpy())}")
                 images = images.to(self.DEVICE)
                 labels = labels.to(self.DEVICE)
-                
+
                 outputs = self.net(images)[:, :self.num_tot_classes]
                 _, preds = torch.max(nn.Softmax(outputs).dim, 1)
-                
+
                 update_confusion_matrix(matrix, preds, labels)
 
                 # Update Corrects
                 running_corrects += torch.sum(preds == labels.data).data.item()
-                
+
             # Calculate Accuracy and mean loss
             accuracy = running_corrects / len(test_dataloader.dataset)
             self.accuracies_FC.append(accuracy)
@@ -348,17 +356,14 @@ class SVMiCaRL():
                 images = images.to(self.DEVICE)
                 fts_map = self.net.feature_extractor(images)
                 preds = self.svc.predict(fts_map.cpu())
-                
+
                 update_confusion_matrix(matrix, torch.Tensor(preds).type_as(labels), labels)
 
                 # Update Corrects
                 running_corrects += torch.sum(torch.Tensor(preds) == labels.data).data.item()
-      
+
             # Calculate Accuracy and mean loss
             accuracy = running_corrects / len(test_dataloader.dataset)
             self.accuracies_SVM.append(accuracy)
             print(f'\033[94mAccuracy on test set with SVM:{accuracy}\x1b[0m')
             show_confusion_matrix(matrix)
-        
-  
-  
